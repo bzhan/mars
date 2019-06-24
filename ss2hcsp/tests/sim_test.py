f0c9ca80051d906166991676e713153e60ca831b
arrowpolar <837529320@qq.com>
2019-06-24 19:41:46 +0800

    update test

--- ss2hcsp/tests/sim_test.py
+++ ss2hcsp/tests/sim_test.py
@@ -8,28 +8,24 @@ from ss2hcsp.hcsp import hcsp
 class SimTest(unittest.TestCase):
     def testVanDerPol1(self):
         diagram = SL_Diagram()
-        diagram.add_block(Integrator("intg1", 1))
-        diagram.add_block(Integrator("intg2", 1))
-        diagram.add_block(Port("in1", "in_port"))
-        diagram.add_block(Port("out1", "out_port"))
+        diagram.add_block(Integrator(name="intg1", init_value=1))
+        diagram.add_block(Integrator(name="intg2", init_value=1))
+        diagram.add_block(Port(name="in1", type="in_port"))
+        diagram.add_block(Port(name="out1", type="out_port"))
         diagram.add_block(Port("out2", "out_port"))
-        diagram.add_block(Port("out3", "out_port"))
-        diagram.add_block(Port("out4", "out_port"))
-        diagram.add_line("in1", "intg1", 0, 0)
-        diagram.add_line("intg1", "out1", 0, 0)
+        diagram.add_line(src="in1", dest="intg1", src_port=0, dest_port=0)
+        diagram.add_line(src="intg1", dest="out1", src_port=0, dest_port=0)
         diagram.add_line("intg1", "intg2", 0, 0)
         diagram.add_line("intg2", "out2", 0, 0)
         diagram.add_line("intg2", "out3", 0, 0)
         diagram.add_line("intg2", "out4", 0, 0)
 
-        hp_init = hcsp.Sequence(hcsp.Assign("x", "1"), hcsp.Assign("y", "1"))
-        out_comms = [hcsp.OutputChannel("chy", "y"),
-                     hcsp.Sequence([hcsp.OutputChannel("chy", "y"),
-                                    hcsp.OutputChannel("chz1", "z"),
-                                    hcsp.OutputChannel("chz2", "z"),
-                                    hcsp.OutputChannel("chz3", "z")])]
+        hp_init = hcsp.Sequence(hcsp.Assign("x", "1"), hcsp.Assign("y", "1"), hcsp.Assign('t', 0))
+        out_comms = [(hcsp.InputChannel("chx", 'x'), hcsp.Skip()),
+                     (hcsp.OutputChannel("chy", 'y'), hcsp.Skip()),
+                     (hcsp.OutputChannel("chz", 'z'), hcsp.Skip())]
         hp_ode = hcsp.ODE_Comm([("y", "x"), ("z", "y"), ("t", "1")], "True", out_comms)
-        hp = hcsp.Loop(Sequence(hcsp.InputChannel("chx", "x"), hp_ode))
+        continuous_hp = hcsp.Sequence(hp_init, hcsp.Loop(hp_ode))
 
 
 if __name__ == "__main__":
