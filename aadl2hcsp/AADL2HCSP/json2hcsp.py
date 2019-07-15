d3671fa2aeb1b4484b631fd32e47bed92d0a7f6d
Bohua Zhan <bzhan@ios.ac.cn>
2019-07-15 23:34:48 +0800

    Move test to unittest.

--- aadl2hcsp/AADL2HCSP/json2hcsp.py
+++ aadl2hcsp/AADL2HCSP/json2hcsp.py
@@ -1,6 +1,6 @@
 #coding=utf-8
 import json
-from hcsp import *
+from aadl2hcsp.AADL2HCSP.hcsp import *
 
 def createStructure(dic):
     lines=[]
@@ -337,30 +337,3 @@ class Thread:
         hp = Defination(hp1, hps)
 
         return hp
-
-
-
-if __name__ == '__main__':
-    file = 'out.json'
-    out=""
-    with open(file, 'r') as f:
-        dic = json.load(f)
-    out += "\n".join(str(line) for line in createStructure(dic))+'\n'
-    out += "\n".join(str(line) for line in createConnections(dic))+'\n'
-
-    for category in dic.values():
-        if category['category'] == 'process' and len(category['components']) > 0:
-            threadlines = []
-            for com in category['components']:
-                if com['category'] == 'thread':
-                    threadlines.append(com['name'])
-            out += "\n".join(str(line) for line in Process(category,threadlines).lines) + '\n'
-
-        elif category['category'] == 'thread':
-            out += "\n".join(str(line) for line in Thread(category).lines) + '\n'
-
-
-    print(out)
-
-
-
