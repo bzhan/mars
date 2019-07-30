cb1a6ed96358da0d78db175e93873ecfe1cd1625
Alchemist <yt4766269@126.com>
2019-07-30 15:31:43 +0800

    hcsp simulator: server

--- ss2hcsp/server/simulator_server.py
+++ ss2hcsp/server/simulator_server.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from flask import request
+import json
 
 app = Flask(__name__)
 
@@ -8,5 +9,14 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = json.loads(request.get_data(as_text=True))
+    hcsp_code = data['code']
+    hcsp_input = data['input']
+    return hcsp_code + hcsp_input
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
