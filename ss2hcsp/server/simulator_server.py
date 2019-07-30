01b5103c5f21057a9af7c2075534920c4205f355
Alchemist <yt4766269@126.com>
2019-07-30 17:19:43 +0800

    hcsp simulator: server

--- ss2hcsp/server/simulator_server.py
+++ ss2hcsp/server/simulator_server.py
@@ -8,6 +8,18 @@ from ss2hcsp.hcsp import parser
 app = Flask(__name__)
 
 
+def postprocess(new_code, hcsp_state, reason):
+    new_code, reason = str(new_code), str(reason)
+
+    new_state = {}
+
+    for key in hcsp_state.keys():
+        val = hcsp_state[key]
+        new_state[key.value] = val
+
+    return new_code, new_state, reason
+
+
 @app.route('/')
 def hello_world():
     return "Hello, World!"
@@ -23,18 +35,27 @@ def process():
 
     new_cmd, reason = simulator.exec_process(cmd, hcsp_input)
 
-    new_state = {}
+    new_code, new_state, reason = postprocess(new_cmd, hcsp_input, reason)
 
-    for key in hcsp_input.keys():
-        val = hcsp_input[key]
-        new_state[key.value] = val
-
-    new_cmd, reason = str(new_cmd), str(reason)
-
-    result = {'new_code': new_cmd, 'new_state': new_state, 'reason': reason}
+    result = {'new_code': new_code, 'new_state': new_state, 'reason': reason}
 
     result = json.dumps(result)
     return result
+
+@app.route('/step', methods=['POST'])
+def step():
+    data = json.loads(request.get_data(as_text=True))
+    hcsp_code = data['code']
+    hcsp_state = data['state']
+
+    hcsp_code = parser.hp_parser.parse(hcsp_code)
+    hcsp_code, reason = simulator.exec_step(hcsp_code, hcsp_state)
+
+    result = {'new_code': hcsp_code, 'new_state': hcsp_state, 'reason': str(reason)}
+
+    result = json.dumps(result)
+    return result
+
 
 
 if __name__ == '__main__':
