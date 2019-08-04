da6d704f6a1ecfbaafe0e16fdba9cf803f29a121
wangjian36 <wangjian36@baidu.com>
2019-08-04 17:25:09 +0800

    debug

--- hcsp_simulator/src/App.js
+++ hcsp_simulator/src/App.js
@@ -4,7 +4,7 @@ import React, {Component} from "react";
 
 import {Nav, Navbar, ButtonToolbar, Button, Container} from "react-bootstrap"
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPlayCircle, faStopCircle, faStepForward, faStepBackward, faForward} from '@fortawesome/free-solid-svg-icons'
+import {faPlayCircle, faStopCircle, faStepForward, faForward, faBackward} from '@fortawesome/free-solid-svg-icons'
 import FlowChart from "./flowChart"
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.main.js'
 import axios from "axios"
@@ -23,6 +23,7 @@ class App extends Component {
                 "}",
             // hcspStates定义：一个数组序列，每一个元素包含当前代码以及当前状态
             hcspStates: [],
+            hcspComm: [],
             started: false
         };
         this.reader = new FileReader();
@@ -69,17 +70,6 @@ class App extends Component {
         this.fileSelector.click();
     };
 
-    nextStep = (e) => {
-        e.preventDefault();
-        this.editor.deltaDecorations([this.decorations],
-            [{range: new monaco.Range(1, 4, 1, 8), options: {inlineClassName: 'myInlineDecoration'}},
-        ])
-    };
-
-    lastStep = (e) => {
-        e.preventDefault();
-    };
-
     run = async (e) => {
         e.preventDefault();
         await this.setStateAsync({hcspCode: this.editor.getValue()});
@@ -122,6 +112,32 @@ class App extends Component {
         this.setState({started: false});
     };
 
+
+    nextStep = async (e) => {
+        e.preventDefault();
+        const tempCode = this.state.hcspStates[this.state.hcspStates.length - 1]['code'];
+        const input = this.state.hcspStates[this.state.hcspStates.length - 1]['state'];
+        const reason = this.state.hcspStates[this.state.hcspStates.length - 1]['reason'];
+        const response = await axios.post("/step", {"code": tempCode, "input": input, "reason": reason});
+        let response_data = response.data;
+        const new_code = response_data['new_code'];
+        const new_state = response_data['new_state'];
+        const new_reason = response_data['reason'];
+        this.state.hcspStates.push(
+            {
+                "code": new_code,
+                "state": new_state,
+                "reason": new_reason
+            }
+        );
+        this.setState({hcspStates: this.state.hcspStates});
+    };
+
+    lastStep = (e) => {
+        e.preventDefault();
+    };
+
+
     setStateAsync = (state) => {
         return new Promise((resolve) => {
             this.setState(state, resolve)
@@ -152,33 +168,22 @@ class App extends Component {
 
                         <Button variant="danger" title={"stop"} onClick={this.stop} disabled={!this.state.started}><FontAwesomeIcon icon={faStopCircle}
                                                                                  size="lg"/></Button>
-                        <Button variant="secondary" title={"forward"} onClick={this.forward} disabled={!this.state.started}><FontAwesomeIcon icon={faForward}
-                                                                                                            size="lg"/></Button>
                         <Button variant="secondary" title={"step forward"} onClick={this.nextStep} disabled={!this.state.started}>
-                            <FontAwesomeIcon icon={faStepForward} size="lg"/>
+                            <FontAwesomeIcon icon={faForward} size="lg"/>
                         </Button>
                         <Button variant="secondary" title={"step backward"} onClick={this.lastStep} disabled={!this.state.started}>
-                            <FontAwesomeIcon icon={faStepBackward} size="lg"/>
+                            <FontAwesomeIcon icon={faBackward} size="lg"/>
                         </Button>
-
+                        <Button variant="secondary" title={"forward"} onClick={this.forward} disabled={!this.state.started}><FontAwesomeIcon icon={faStepForward}
+                                                                                                                                             size="lg"/></Button>
                     </ButtonToolbar>
                 </div>
                 <hr/>
-                {/*<Container style={{"maxWidth": window.innerWidth}}>*/}
-                {/*    <Row>*/}
-                {/*        <Col>*/}
-                {/*            <div id="monaco-editor" style={{width: window.innerWidth / 2.2, height: 750}}/>*/}
-                {/*        </Col>*/}
-                {/*        <div className="vl"/>*/}
-                {/*        <Col><FlowChart hcspStates={this.state.hcspStates}/></Col>*/}
-                {/*    </Row>*/}
-                {/*</Container>*/}
                 <Container style={{"maxWidth": window.innerWidth}}>
                     <div id="monaco-editor" style={{height: 250}}/>
                     <hr/>
                     <FlowChart style={{"maxWidth": window.innerWidth}} hcspStates={this.state.hcspStates}/>
                 </Container>
-
             </div>
         );
     }
