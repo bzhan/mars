322ba15449e39c104d3f8ef0f40f964d59d045b1
arrowpolar <837529320@qq.com>
2019-09-03 23:15:47 +0800

    Modify simulink_isollete.xml

--- ss2hcsp/sl/port.py
+++ ss2hcsp/sl/port.py
@@ -1,16 +1,18 @@
 from ss2hcsp.sl.sl_block import SL_Block
+from ss2hcsp.hcsp.expr import AVar
 
 
 class Port(SL_Block):
     """Represents an input/output channel for the overall diagram."""
     def __init__(self, name, port_type):
+        super(Port, self).__init__()
         """port_type specifies whether it is an input or output channel.
         'in_port' means the block has a src line entering the diagram.
         'out_port' means the block has a dest line leaving the diagram.
 
         """
         self.name = name
-        assert port_type == 'in_port' or port_type == 'out_port'
+        assert port_type in ['in_port', 'out_port']
         self.type = port_type
 
         if self.type == 'in_port':
@@ -18,11 +20,13 @@ class Port(SL_Block):
             self.src_lines = [[]]
             self.num_dest = 0
             self.dest_lines = []
+            self.st = AVar(name=self.name)
         else:
             self.num_src = 0
             self.src_lines = []
             self.num_dest = 1
             self.dest_lines = [None]
+            self.st = -1
 
     def __str__(self):
         if self.type == 'in_port':
