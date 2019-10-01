b4f662be0295d6ba83ae6d57f8de0c0525d6edfb
Bohua Zhan <bzhan@ios.ac.cn>
2019-10-01 20:16:01 +0800

    Add error margin for comparisons.

--- ss2hcsp/hcsp/simulator.py
+++ ss2hcsp/hcsp/simulator.py
@@ -121,17 +121,23 @@ def eval_expr(expr, state):
         a = eval_expr(expr.expr1, state)
         b = eval_expr(expr.expr2, state)
         if expr.op == "<":
-            return a < b
+            return b - a > 1e-7
         elif expr.op == ">":
-            return a > b
+            return a - b > 1e-7
         elif expr.op == "==":
-            return a == b
+            if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+                return abs(a - b) < 1e-7
+            else:
+                return a == b
         elif expr.op == "!=":
-            return a != b
+            if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+                return abs(a - b) > 1e-7
+            else:
+                return a != b
         elif expr.op == ">=":
-            return a >= b
+            return a - b > -1e-7
         elif expr.op == "<=":
-            return a <= b
+            return b - a > -1e-7
         else:
             raise NotImplementedError
 
