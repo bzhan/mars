79fd282c298d2f85d58bdd56729bb17ad4756128
Bohua Zhan <bzhan@ios.ac.cn>
2020-06-01 16:37:34 +0800

    Update watertank.

--- lunarlander_sl/Watertank.thy
+++ lunarlander_sl/Watertank.thy
@@ -9,10 +9,10 @@ locale watertank =
     g :: real and
     period :: real
   assumes
-    Qmax_val: "Qmax = 0.007" and
-    r_val: "r = 0.0254" and
-    g_val: "g = 9.8" and
-    period_val: "period = 0.01"
+    Qmax_def: "Qmax = 0.007" and
+    r_def: "r = 0.0254" and
+    g_def: "g = 9.8" and
+    period_def: "period = 0.01"
 begin
 
 definition H :: char where "H = CHR ''h''"
@@ -40,9 +40,15 @@ definition controller :: proc where
 definition model :: pproc where
   "model = PProc [plant, controller]"
 
+text \<open>Invariant for plant at start of loop\<close>
+definition Inv1 :: "state \<Rightarrow> bool" where
+  "Inv1 s \<longleftrightarrow> (s H \<ge> 0.3 \<and> s H \<le> 0.6 \<and> (s H \<le> 0.31 \<longrightarrow> s V = 1) \<and> (s H \<ge> 0.59 \<longrightarrow> s V = 0))"
+
+text \<open>Invariant for plant after running the ODE\<close>
+definition Inv2 :: "state \<Rightarrow> bool" where
+  "Inv2 s \<longleftrightarrow> (s H \<ge> 0.3 \<and> s H \<le> 0.6)"
+
 
 end
-
-
 
 end
