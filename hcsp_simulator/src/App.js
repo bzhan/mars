de7542838df1a62a5b6d416b36532803a255ca7d
Bohua Zhan <bohuazhan@gmail.com>
2021-02-28 10:15:04 +0800

    Fix display of strings.

--- hcsp_simulator/src/App.js
+++ hcsp_simulator/src/App.js
@@ -86,6 +86,10 @@ class Process extends React.Component {
             // Round numbers to at most three digits
             return String(Math.round(val.toFixed(3) * 1000) / 1000);
         }
+        else if (typeof (val) == 'string') {
+            // Display of strings
+            return val;
+        }
         else if (Array.isArray(val)) {
             // Display of lists
             return '[' + val.map((k) => this.displayValue(k)).join(',') + ']';
