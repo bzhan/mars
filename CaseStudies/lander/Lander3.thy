7450cac6b932ab88c09cc391ae10653f85518fe8
Alexander Bentkamp <bentkamp@gmail.com>
2022-06-22 13:42:07 +0200

    commit test output

--- CaseStudies/lander/Lander3.thy
+++ CaseStudies/lander/Lander3.thy
@@ -18,7 +18,7 @@ definition P0 :: proc where
       Cm (''ch_Fc_a''[?]Fc);
       Cm (''ch_v''[?]V);
       Cm (''ch_m''[?]M);
-      Fc ::= (\<lambda>s. (-(s Fc / s M - 3.732) * 0.01 + 3.732 - (s V - (-1.5)) * 0.6) * s M);
+      Fc ::= (\<lambda>s. (-(s Fc / s M - 3.732) * 0.01 + 3.732 - (s V - -1.5) * 0.6) * s M);
       Cm (''ch_Fc_b''[!](\<lambda>s. s Fc));
       Cm (''ch_Fc''[!](\<lambda>s. s Fc));
       Wait ((\<lambda>_. 0.128))
@@ -29,7 +29,7 @@ definition P1 :: proc where
     V ::= (\<lambda>_. -1.5);
     M ::= (\<lambda>_. 759.5);
     Rep (
-      Interrupt (ODE ((\<lambda>_ _. 0)(V := (\<lambda>s. s Fc / s M - 3.732), M := (\<lambda>s. s Fc / 2500 * (-1))))) ((\<lambda>_. True))
+      Interrupt (ODE ((\<lambda>_ _. 0)(V := (\<lambda>s. s Fc / s M - 3.732), M := (\<lambda>s. s Fc / 2500 * -1)))) ((\<lambda>_. True))
       [(''ch_v''[!](\<lambda>s. s V), Skip), (''ch_m''[!](\<lambda>s. s M), Skip), (''ch_Fc''[?]Fc, Skip)]
     )"
 
