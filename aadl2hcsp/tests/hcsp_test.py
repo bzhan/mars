f0ababaa8f4bd3c9cf8a47277be8b065c5095996
Bohua Zhan <bzhan@ios.ac.cn>
2022-10-20 11:44:35 +0800

    Fix syntax errors.

--- aadl2hcsp/tests/hcsp_test.py
+++ aadl2hcsp/tests/hcsp_test.py
@@ -7,17 +7,12 @@ class HCSPTest(unittest.TestCase):
     def testCCS(self):
         with open("Examples\AADL\CCS\TCS\generatedcode\system.txt", "r") as f:
             text = f.read()
-        try:
-            if text.startswith('%type: module'):
-                infos = parser.parse_module_file(text)
-            else:
-                infos = parser.parse_file(text)
-        except parser.ParseFileException as e:
-            print(e.error_msg)
+
+        if text.startswith('%type: module'):
+            infos = parser.parse_module_file(text)
+        else:
+            infos = parser.parse_file(text)
 
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
