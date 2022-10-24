857b34dd76f79c0367c3d0fd9eefcb0cb05492bc
Bohua Zhan <bzhan@ios.ac.cn>
2022-10-24 21:42:57 +0800

    Add simulator tests 3-10

--- hcsp2c/tests/hcsp2c_test.py
+++ hcsp2c/tests/hcsp2c_test.py
@@ -35,7 +35,10 @@ class HCSP2CTest(unittest.TestCase):
             shell=True
         )
 
-        self.assertEqual(res.stdout, '\n'.join(expected_output) + '\n')
+        if not expected_output:
+            self.assertEqual(res.stdout, "")
+        else:
+            self.assertEqual(res.stdout, '\n'.join(expected_output) + '\n')
 
 
     def testa(self):
@@ -114,6 +117,7 @@ class HCSP2CTest(unittest.TestCase):
         self.run_file(progs, "teste", expected_output, step_size=1e-5)
 
     def test1(self):
+        # TODO: support c2p!x-1 rather than y := x-1; c2p!y
         progs = [
             "x := 0; { {x_dot = 1 & true} |> [](p2c!x --> skip;) c2p?x; }*",
             "x := 0; y := 0; { wait(2); p2c?x; y := x-1; c2p!y; }*"
@@ -141,6 +145,111 @@ class HCSP2CTest(unittest.TestCase):
 
         self.run_file(progs, "test2", expected_output, step_size=1e-1)
 
+    def test3(self):
+        progs = [
+            "x := 0; x := x + 1;",
+            "y := 2;",
+        ]
+
+        expected_output = [
+            "deadlock"
+        ]
+
+        self.run_file(progs, "test3", expected_output, step_size=1e-1)
+
+    def test4(self):
+        progs = [
+            "x := 0; { {x_dot = 1 & true} |> [](p2c!x --> skip;) c2p?x; }*",
+            "x := 0; { {x_dot = 1 & true} |> [](p2c!x --> skip;) c2p?x; }*",
+        ]
+
+        expected_output = [
+        ]
+
+        self.run_file(progs, "test4", expected_output, step_size=1e-1)
+
+    def test5(self):
+        # TODO: changing channel names from chx, chz, chy to x, z, y reveals a bug.
+        progs = [
+            "x := 0; y := 0; z := 1; {chx?x --> skip; $ chz!z --> skip; $ chy?y --> skip;}",
+            "y := 2; chy!y;",
+        ]
+
+        expected_output = [
+            "IO chy 2.000",
+            "deadlock"
+        ]
+
+        self.run_file(progs, "test5", expected_output, step_size=1e-1)
+
+    def test6(self):
+        progs = [
+            "x := 0; y := 0; z := 1; {chx?x --> skip; $ chz!z --> skip; $ chy?y --> skip;}",
+            "z := 0; chz?z;",
+        ]
+
+        expected_output = [
+            "IO chz 1.000",
+            "deadlock"
+        ]
+
+        self.run_file(progs, "test6", expected_output, step_size=1e-1)
+
+    def test7(self):
+        progs = [
+            "x := 1; y := 2; z := 3; wait(3); w := 4;",
+            "x := 11; y := 12; wait(2); z := 3;"
+        ]
+
+        expected_output = [
+            "deadlock"
+        ]
+
+        self.run_file(progs, "test7", expected_output, step_size=1e-1)
+
+    def test8(self):
+        # TODO: changing chy to chx reveals a bug.
+        # We can make an assumption that each direction of communication channel is used
+        # by only one process.
+        progs = [
+            "x := 0; y := 0; {chx?x --> y := x+1; chy!y; $ chy?y --> skip;} y := x+2; chx!y;",
+            "x := 0; y := 3; chx!y; chy?x; chx?x;"
+        ]
+
+        expected_output = [
+            "IO chx 3.000",
+            "IO chy 4.000",
+            "IO chx 5.000",
+            "deadlock"
+        ]
+
+        self.run_file(progs, "test8", expected_output, step_size=1e-1)
+
+    def test9(self):
+        progs = [
+            "x := 0; v := 0; a := 1; {x_dot = v, v_dot = a & true} |> [](c!x --> skip;)",
+            "x := 0; wait(3); c?x;"
+        ]
+
+        expected_output = [
+            "IO c 4.500",
+            "deadlock"
+        ]
+
+        self.run_file(progs, "test9", expected_output, step_size=1e-5)
+
+    def test10(self):
+        progs = [
+            "x := 0; v := 0; a := 1; {x_dot = v, v_dot = a & x < 3} c!x;",
+            "x := 0; c?x;"
+        ]
+
+        expected_output = [
+            "IO c 3.000",
+            "deadlock"
+        ]
+
+        self.run_file(progs, "test10", expected_output, step_size=1e-5)
 
 
 if __name__ == "__main__":
